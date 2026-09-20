6771a3e490422049c540f6bf50adff9607e040a0
mikesplore <mikepremium8@gmail.com>
2026-09-20 18:42:54 +0300

    Animate side panel transitions

--- src/components/ui/dialog.tsx
+++ src/components/ui/dialog.tsx
@@ -14,7 +14,7 @@ const DialogOverlay = React.forwardRef<
 >(({ className, ...props }, ref) => (
   <DialogPrimitive.Overlay
     ref={ref}
-    className={cn("fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out", className)}
+    className={cn("fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=open]:fade-in data-[state=closed]:fade-out", className)}
     {...props}
   />
 ));
